a62d539b1fa90d36e2a4f2fc21384716318b0347
j2ho <92702929+j2ho@users.noreply.github.com>
2025-08-20 15:23:11 +0900

    initial commit

---
+++ hproteome-bsite/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for sitedb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path = '/home/j2ho/django_all'
+if path not in sys.path:
+    sys.path.append(path) 
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sitedb.settings')
+
+application = get_wsgi_application()
